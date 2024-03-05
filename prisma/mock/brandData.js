ee1c0dc6c836ed705b01130b8349e9301cfdf3cf
kvinbindai <kavinsanguansook@gmail.com>
2024-03-05 20:26:30 +0700

    add picture and add column in brand table and seed newBrand

--- prisma/mock/brandData.js
+++ prisma/mock/brandData.js
@@ -1,8 +1,30 @@
 module.exports.brandData = [
-  {  name: "Rolex" },
-  {  name: "Omega" },
-  {  name: "Seiko" },
-  {  name: "Cartier" },
-  {  name: "Hublot" },
-  {  name: "Casio" },
+  {
+    name: "Rolex",
+    brandImage: "tps://logowik.com/content/uploads/images/275_rolex.jpg",
+  },
+  {
+    name: "Omega",
+    brandImage: "tps://logowik.com/content/uploads/images/434_omega.jpg",
+  },
+  {
+    name: "Seiko",
+    brandImage:
+      "https://logowik.com/content/uploads/images/seiko-watch2671.jpg",
+  },
+  {
+    name: "Cartier",
+    brandImage:
+      "https://logowik.com/content/uploads/images/cartier-wordmark4825.logowik.com.webp",
+  },
+  {
+    name: "Hublot",
+    brandImage:
+      "https://logowik.com/content/uploads/images/hublot3517.logowik.com.webp",
+  },
+  {
+    name: "Casio",
+    brandImage:
+      "https://logowik.com/content/uploads/images/casio8798.logowik.com.webp",
+  },
 ];
